2fe9d949707b0e3eb4e1c0f8baade6d59bd8cda7
harshgupta9321 <112786943+harshgupta9321@users.noreply.github.com>
2024-11-02 17:51:31 +0530

    mails in build

--- client/redux/features/auth/authApi.ts
+++ client/redux/features/auth/authApi.ts
@@ -1,120 +1,115 @@
-
 import { apiSlice } from "../api/apiSlice";
-import { userRegistration,userLoggedIn, userLoggedOut } from "./authSlice";
-
-
+import { userRegistration, userLoggedIn, userLoggedOut } from "./authSlice";
 
 type RegistrationResponse = {
-    message: string;
-    activationToken: string;
+  message: string;
+  activationToken: string;
 };
 
-type RegistrationData = {  };
+type RegistrationData = {};
 
-
-
-
-
-export const authApi:any = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        register: builder.mutation<RegistrationResponse, RegistrationData>({
-            query: (data) => ({
-                url: "registration",
-                method: "POST",
-                body: data,
-                credentials: "include" as const,
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userRegistration({
-                            token: result.data.activationToken,
-                        })
-                    );
-                } catch (error) {
-                    console.error('Error:', error);
-                }
-            },
-        }),
-        activation: builder.mutation({
-            query: ({ activation_token, activation_code }) => ({
-                url: "activate-user",
-                method: "POST",
-                body: {
-                    activation_token,
-                    activation_code,
-                },
-            }),
-        }),
-        login:builder.mutation({
-            query:({email,password})=>({
-                url:"login-user",
-                method:"POST",
-                body:{
-                    email,
-                    password
-                },
-                credentials:"include" as const
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userLoggedIn({
-                            accessToken: result.data.accessToken,
-                            user:result.data.user
-                        })
-                    );
-                } catch (error) {
-                    console.error('Error:', error);
-                }
-            }
-        }),
-        socialAuth:builder.mutation({
-            query:({email,name,avatar})=>({
-                url:"social-auth",
-                method:"POST",
-                body:{
-                    email,
-                    name,
-                    avatar
-                },
-                credentials:"include" as const
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    const result = await queryFulfilled;
-                    dispatch(
-                        userLoggedIn({
-                            accessToken: result.data.accessToken,
-                            user:result.data.user
-                        })
-                    );
-                } catch (error) {
-                    console.error('Error:', error);
-                }
-            }
-        }),
-        logOut:builder.query({
-            query:()=>({
-                url:"logout-user",
-                method:"GET",
-                credentials:"include" as const
-            }),
-            async onQueryStarted(arg, { queryFulfilled, dispatch }) {
-                try {
-                    
-                    dispatch(
-                        userLoggedOut()
-                    );
-                } catch (error) {
-                    console.error('Error:', error);
-                }
-            }
-        })
+export const authApi: any = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "registration",
+        method: "POST",
+        body: data,
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
+            })
+          );
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      },
     }),
+    activation: builder.mutation({
+      query: ({ activation_token, activation_code }) => ({
+        url: "activate-user",
+        method: "POST",
+        body: {
+          activation_token,
+          activation_code,
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: ({ email, password }) => ({
+        url: "login-user",
+        method: "POST",
+        body: {
+          email,
+          password,
+        },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      },
+    }),
+    socialAuth: builder.mutation({
+      query: ({ email, name, avatar }) => ({
+        url: "social-auth",
+        method: "POST",
+        body: {
+          email,
+          name,
+          avatar,
+        },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      },
+    }),
+    logOut: builder.query({
+      query: () => ({
+        url: "logout-user",
+        method: "GET",
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          dispatch(userLoggedOut());
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      },
+    }),
+  }),
 });
 
-
-export const {useRegisterMutation,useActivationMutation,useLoginMutation,useSocialAuthMutation,useLogOutQuery}=authApi
+export const {
+  useRegisterMutation,
+  useActivationMutation,
+  useLoginMutation,
+  useSocialAuthMutation,
+  useLogOutQuery,
+} = authApi;
